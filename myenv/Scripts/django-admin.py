26b061eb9e3791d2b2391348bd35620fcfe3851a
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-23 00:45:04 +0200

    initial commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Student\Documents\Sihlobile Styles\django-ecommerce\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
